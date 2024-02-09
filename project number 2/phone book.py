138d58c91352442db5a89e082761906e15c6c6fe
sanifatima <156574849+sanifatima@users.noreply.github.com>
2024-02-09 18:24:50 +0500

    Add files via upload

---
+++ project number 2/phone book.py
@@ -0,0 +1,64 @@
+# amna muhammad essa
+# essaamna101@gmail.com
+# phone book
+
+phone_number = []
+
+num = 2
+for i in range(num):
+    name = input("enter the name")
+    phone = input("enter the phone number")
+print(f"contact{name} with number{num} added.") 
+
+def find_contact(phone_book,name):
+    if name in phone_book:
+        print(f"the number of{name} is{phone_book[name]}.")
+    else:
+        print(f"sorry, {name} is not found in phone_book.")
+
+    def search_contact(self,name):
+        if name in self.contacts:
+            print(f"{name}'s number is: {self.contacts[name]}")
+        else:
+         print(f"Contact {name} not found in the phone book.")
+     
+        
+
+
+
+
+        
+
+        
+
+        
+
+
+    
+                
+   
+
+
+
+
+
+     
+    
+
+    
+
+
+
+
+     
+
+
+
+
+
+
+
+
+
+
+    
